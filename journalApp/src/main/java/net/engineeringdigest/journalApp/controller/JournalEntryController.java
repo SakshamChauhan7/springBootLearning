5f3f9eae85c0c41c37ed80e0a130f0e2f8d3eb9d
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-16 12:50:16 -0500

    learning basic functionality of springboot annotations

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -1,9 +1,43 @@
 package net.engineeringdigest.journalApp.controller;
 
+import java.util.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return  new ArrayList<>(journalEntries.values()) ;
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry delEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId,myEntry);
+    }
 
 }
