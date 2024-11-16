d2565f9603ea549e991b0287d1d7dcd8ddd05365
SakshamChauhan07 <113049023+SakshamChauhan07@users.noreply.github.com>
2024-11-16 14:29:53 -0500

    adding new controller and service, changing id from long to string

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
@@ -9,35 +9,33 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/journal")
-public class JournalEntryController {
+public class JournalEntryControllerV2 {
 
 
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
     @GetMapping
     public List<JournalEntry> getAll(){
-        return  new ArrayList<>(journalEntries.values()) ;
+        return null;
     }
 
     @PostMapping
     public boolean createEntry(@RequestBody JournalEntry myEntry){
-        journalEntries.put(myEntry.getId(),myEntry);
         return true;
     }
 
     @GetMapping("id/{myId}")
     public JournalEntry getJournalEntryById(@PathVariable Long myId){
-        return journalEntries.get(myId);
+        return null;
     }
 
     @DeleteMapping("id/{myId}")
     public JournalEntry delEntryById(@PathVariable Long myId){
-        return journalEntries.remove(myId);
+        return null;
     }
 
     @PutMapping("id/{myId}")
     public JournalEntry updateJournalById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
-        return journalEntries.put(myId,myEntry);
+        return null;
     }
 
 }
